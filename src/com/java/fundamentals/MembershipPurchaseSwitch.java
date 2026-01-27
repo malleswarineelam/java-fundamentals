adce31bc6121d632e74554b877292bf79018214d
malleswarineelam <neelammalleswari410@gmail.com>
2026-01-27 15:03:13 +0530

    switch usecase is added

---
+++ src/com/java/fundamentals/MembershipPurchaseSwitch.java
@@ -0,0 +1,53 @@
+package com.java.fundamentals;
+
+import java.util.Scanner;
+
+public class MembershipPurchaseSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float productPrice, discountPercentage, discountAmount;
+        float finalPrice, finalQuantityPrice, totalSavedAmount;
+        int quantity, shippingCharges;
+        float centralTax = 2.5F;
+        float stateTax = 2.5F;
+        char memberShip;
+        System.out.println("Enter Product Price : ");            //variables declaration
+        productPrice = sc.nextFloat();
+        System.out.println("Enter Membership : ");
+        memberShip = Character.toUpperCase(sc.next().charAt(0));
+        switch (memberShip) {
+            case 'S':
+                discountPercentage = 10;
+                shippingCharges = 50;
+                break;
+            case 'G':
+                discountPercentage = 15;
+                shippingCharges = 20;
+                break;
+            case 'D':
+                discountPercentage = 20;
+                shippingCharges = 0;
+                break;
+            default:
+                System.out.println("No Membership");
+                discountPercentage = 0;
+                shippingCharges = 100;
+                break;
+        }
+        discountAmount = productPrice * discountPercentage / 100;
+        finalPrice = productPrice - discountAmount + (centralTax + stateTax) + shippingCharges;
+        System.out.println("Discount Amount : " + discountAmount);
+        System.out.println("Central Tax : " + centralTax);
+        System.out.println("State Tax : " + stateTax);
+        System.out.println("MemberShip of Customer is :  " + memberShip);
+        System.out.println("Shipping charges : " + shippingCharges);
+        System.out.println("final Price of 1 product : " + finalPrice);
+        System.out.println("Enter Quantity : ");
+        quantity = sc.nextInt();
+        finalQuantityPrice = quantity * finalPrice;
+        System.out.println("Final Bill : " + finalQuantityPrice);
+        totalSavedAmount = (productPrice * quantity) - finalQuantityPrice;
+        System.out.println("Total amount saved :" + totalSavedAmount);
+        sc.close();
+    }
+}
