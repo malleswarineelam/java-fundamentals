c3709da16a23ebb7d4f2a7ccc37f9a0919e0f4d4
malleswarineelam <neelammalleswari410@gmail.com>
2026-01-27 21:06:29 +0530

    question 15  added

---
+++ src/com/java/codingquestions/CheckArmstrong.java
@@ -0,0 +1,32 @@
+package com.java.codingquestions;
+import java.util.Scanner;
+//15.check whether number is Armstrong or not
+public class CheckArmstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int N = sc.nextInt();
+        int count = 0;
+        int sum = 0;
+        int digit;
+        int temp = N;
+
+        while (temp > 0) {
+            digit = temp % 10;
+            count = count + 1;
+            temp=temp/10;
+        }
+        temp = N;
+        while (temp > 0) {
+            digit = temp % 10;
+            sum +=(int) Math.pow(digit, count);
+            temp = temp / 10;
+        }
+        if (sum == N)
+            System.out.println("ArmStrong Number");
+        else
+            System.out.println("Not a ArmStrong Number");
+
+        sc.close();
+    }
+}
