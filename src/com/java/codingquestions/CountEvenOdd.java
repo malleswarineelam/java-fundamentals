0101654ab6b1dc648bd3eb5fbc375471584d575c
malleswarineelam <neelammalleswari410@gmail.com>
2026-01-28 11:18:10 +0530

    coding questions 16,17,18 are added

---
+++ src/com/java/codingquestions/CountEvenOdd.java
@@ -0,0 +1,28 @@
+package com.java.codingquestions;
+
+import java.util.Scanner;
+
+//18.Count Even and Odd digits in a number
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number N: ");
+        int N = sc.nextInt();
+        int countEven = 0;
+        int countOdd = 0;
+        int digit;
+        while (N > 0) {
+            digit = N % 10;
+            if (digit % 2 == 0) {
+                countEven = countEven + 1;
+            } else {
+                countOdd = countOdd + 1;
+            }
+            N = N / 10;
+        }
+        System.out.println("Count of Even digits in a number : " + countEven);
+        System.out.println("Count of Odd digits in a number : " + countOdd);
+        sc.close();
+    }
+
+}
