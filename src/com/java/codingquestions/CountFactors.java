29a5c56a60caece9974e5c66b6abaccdc013e16b
malleswarineelam <neelammalleswari410@gmail.com>
2026-01-28 16:08:53 +0530

    question 23 is  added

---
+++ src/com/java/codingquestions/CountFactors.java
@@ -0,0 +1,19 @@
+package com.java.codingquestions;
+//23.count no of factors of a number
+import java.util.Scanner;
+
+public class CountFactors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int N = sc.nextInt();
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                count = count + 1;
+            }
+        }
+        System.out.println("count of factors of given number:" + count);
+        sc.close();
+    }
+}
