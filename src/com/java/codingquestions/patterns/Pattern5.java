ae00dd866402e91ef2f4ea4e16925534eb0648a0
malleswarineelam <neelammalleswari410@gmail.com>
2026-02-01 21:35:08 +0530

    patterns are added-new

---
+++ src/com/java/codingquestions/patterns/Pattern5.java
@@ -0,0 +1,25 @@
+package com.java.codingquestions.patterns;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter n: ");
+                int n = sc.nextInt();
+                int i = 1;
+                while (n >= i) {
+                    int j = 1;
+                    while (n >= j) {
+                        System.out.print(" " + "*" + " ");
+                        j++;
+                    }
+                    System.out.println(" ");
+                    n--;
+                }
+                sc.close();
+
+            }
+        }
+
+
